29a9b3271881ea94da6d128316268f5c7fc4f2a1
Narvatov <hipi96222@gmail.com>
2024-12-16 00:42:12 +0000

    Run only with admin privileges. Set up unit-tests finished

--- TCP_tests/TCPHandshakeTest.cpp
+++ TCP_tests/TCPHandshakeTest.cpp
@@ -2,10 +2,12 @@
 // Created by o.narvatov on 12/10/2024.
 //
 #include <catch2/catch_all.hpp>
+#include <catch2/internal/catch_preprocessor_internal_stringify.hpp>
 
 #include "SimpleTCP.h"
 
-TEST_CASE("ExampleDate", "[TCPHandshakeTest]") {
+TEST_CASE("ExampleDate", "[TCPHandshakeTest]")
+ {
     CHECK(true);
 
     SimpleTCP simpleTcp {};
